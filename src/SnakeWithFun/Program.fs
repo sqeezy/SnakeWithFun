8f51ca174148b09b17cf577ddec344a3b0afaded
Anton Herzog <info@antonherzog.com>
2025-06-21 16:42:13 +0200

    Add food and growing

--- src/SnakeWithFun/Program.fs
+++ src/SnakeWithFun/Program.fs
@@ -1,8 +1,8 @@
 ﻿type Position = { X: int; Y: int }
-type Snake = { Length: int; Position: Position list }
+type Snake = { Position: Position list }
 type Grid = { Width: int; Height: int }
 type Direction = Up | Down | Left | Right
-type GameState = { Snake: Snake; Grid: Grid; CurrentDirection: Direction }
+type GameState = { Snake: Snake; Grid: Grid; CurrentDirection: Direction; FoodPosition: Position }
 type UserInput =
     | DirectionChange of Direction
     | Idle
@@ -13,22 +13,20 @@ let wrappedPositionUpdate (pos: Position) (grid: Grid) =
     let wrappedY = if pos.Y < 0 then grid.Height - 1 elif pos.Y >= grid.Height then 0 else pos.Y
     { X = wrappedX; Y = wrappedY }
 
-let moveSnake (snake: Snake) (direction: Direction) (grid : Grid) =
-    let head = List.head snake.Position
-    let newHead =
-        match direction with
-        | Up -> { X = head.X; Y = head.Y - 1 }
-        | Down -> { X = head.X; Y = head.Y + 1 }
-        | Left -> { X = head.X - 1; Y = head.Y }
-        | Right -> { X = head.X + 1; Y = head.Y }
-    
-    let newPosition = newHead :: List.take (snake.Length - 1) snake.Position |> List.map (fun pos -> wrappedPositionUpdate pos grid)
-    { snake with Position = newPosition }
-    
 let updatedDirection (current: Direction) (userRequest: Direction) =
     match (current, userRequest) with
     | (Up, Down) | (Down, Up) | (Left, Right) | (Right, Left) -> current // No change if opposite direction requested
     | (_, _) -> userRequest // Update to new direction if valid
+    
+let findNewFoodPosition (snake: Snake) (grid: Grid) =
+    let random = System.Random()
+    let rec generatePosition () =
+        let pos = { X = random.Next(0, grid.Width); Y = random.Next(0, grid.Height) }
+        if List.exists (fun sPos -> sPos.X = pos.X && sPos.Y = pos.Y) snake.Position then
+            generatePosition () // Regenerate if position is occupied by the snake
+        else
+            pos
+    generatePosition ()
     
 let gameTick (state: GameState) (input: UserInput) =
     let newDirection =
@@ -36,11 +34,29 @@ let gameTick (state: GameState) (input: UserInput) =
         | DirectionChange dir -> updatedDirection state.CurrentDirection dir
         | Idle -> state.CurrentDirection
         | Quit -> failwith "Game Over"
+        
+    let snake = state.Snake
+    let head = List.head snake.Position
+    let newHead =
+        match newDirection with
+        | Up -> { X = head.X; Y = head.Y - 1 }
+        | Down -> { X = head.X; Y = head.Y + 1 }
+        | Left -> { X = head.X - 1; Y = head.Y }
+        | Right -> { X = head.X + 1; Y = head.Y }
     
-    let newSnake = moveSnake state.Snake newDirection state.Grid
-    { state with Snake = newSnake; CurrentDirection = newDirection }
+    let (eatingManipulation,newFoodPosition) =
+        if newHead = state.FoodPosition then
+            // If the snake eats food, increase its length
+            (0, findNewFoodPosition snake state.Grid)
+        else
+            // If not eating, remove the tail segment
+            (1, state.FoodPosition)
+    let newPosition = newHead :: List.take (snake.Position.Length - eatingManipulation) snake.Position |> List.map (fun pos -> wrappedPositionUpdate pos state.Grid)
+    
+    let newSnake = { snake with Position = newPosition }
+    { state with Snake = newSnake; CurrentDirection = newDirection; FoodPosition = newFoodPosition }
 
-let printState {Snake=snake;Grid=grid} =
+let printState {Snake=snake;Grid=grid;FoodPosition=foodPosition} =
     // print border around the grid
     for _ in 0 .. grid.Width + 1 do
         printf "-"
@@ -51,7 +67,11 @@ let printState {Snake=snake;Grid=grid} =
         printf "|"
         for x in 0 .. grid.Width - 1 do
             if List.exists (fun pos -> pos.X = x && pos.Y = y) snake.Position then
-                printf "S"
+                printf "X"
+            elif x = snake.Position.Head.X && y = snake.Position.Head.Y then
+                printf "F"
+            elif x = foodPosition.X && y = foodPosition.Y then
+                printf "O"
             else
                 printf " "
         printf "|"
@@ -88,8 +108,8 @@ let rec gameLoop state =
     | ex -> printfn "Error: %s" ex.Message
     
 let grid = { Width = 20; Height = 10 }
-let initialSnake = { Length = 3; Position = [{ X = 1; Y = 1 }; { X = 1; Y = 2 }; { X = 1; Y = 3 }] }
-let initialState = { Snake = initialSnake; Grid = grid; CurrentDirection = Right }
+let initialSnake = { Position = [{ X = 1; Y = 1 }; { X = 1; Y = 2 }; { X = 1; Y = 3 }] }
+let initialState = { Snake = initialSnake; Grid = grid; CurrentDirection = Right; FoodPosition = { X = 5; Y = 5 } }
     
 // we need sta thread here to use WPF input
 [<EntryPoint>]
